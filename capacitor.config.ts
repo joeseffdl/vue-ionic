93436da69f1cef59d527ecf476a8a99222b09c70
Joseph De Leon <joeseffdl@gmail.com>
2024-10-26 11:50:42 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vue-ionic-capacitor',
+  webDir: 'dist'
+};
+
+export default config;
